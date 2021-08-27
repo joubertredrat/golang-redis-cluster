4a56347ca787997f0d4d3ddbf7e126a3561e4936
Joubert RedRat <eu+git@redrat.com.br>
2021-08-27 15:03:44 -0300

    Add source

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/joubertredrat/golang-redis-cluster
+
+go 1.17
+
+require (
+	github.com/go-redis/cache/v8 v8.4.1
+	github.com/go-redis/redis/v8 v8.11.3
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/klauspost/compress v1.12.2 // indirect
+	github.com/vmihailenco/bufpool v0.1.11 // indirect
+	github.com/vmihailenco/go-tinylfu v0.2.0 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.1.0 // indirect
+	github.com/vmihailenco/tagparser v0.1.2 // indirect
+	golang.org/x/exp v0.0.0-20201221025956-e89b829e73ea // indirect
+	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a // indirect
+)
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-redis/cache/v8"
+	"github.com/go-redis/redis/v8"
+)
+
+func main() {
+	fmt.Println("0")
+
+	rdb := redis.NewClusterClient(
+		&redis.ClusterOptions{
+			Addrs: []string{"127.0.0.1:6379", "127.0.0.1:6380", "127.0.0.1:6381"},
+		},
+	)
+
+	fmt.Println("1")
+
+	rdb.Ping(context.Background())
+
+	fmt.Println("2")
+
+	client := cache.New(
+		&cache.Options{
+			Redis:      rdb,
+			LocalCache: cache.NewTinyLFU(1000, time.Second),
+		},
+	)
+
+	fmt.Println("3")
+
+	for i := 0; i < 3; i++ {
+		err := client.Set(
+			&cache.Item{
+				Key:   fmt.Sprintf("key_%d", i),
+				Value: fmt.Sprintf("key_%d", i),
+			},
+		)
+		fmt.Println(err)
+	}
+
+	fmt.Println("4")
+}
